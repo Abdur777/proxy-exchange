06e0f5bcf3756fb1a982364a7f29d47bb38cfdae
Abdur Rafay <abdurrafay0123@gmail.com>
2025-02-02 11:57:12 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,11 @@ app.get('/tickers', async (req: Request, res: Response) => {
 app.get('/kline', async (req: Request, res: Response) => {
     try {
         const market = req.query.symbol as string;
-        // console.log("market");
-        const response = await fetch(`https://api.backpack.exchange/api/v1/klines?symbol=${market}&interval=15m&startTime=1737909000`);
+        const currentDate = new Date();
+        currentDate.setMonth(currentDate.getMonth() - 1);
+        const startTime = Math.floor(currentDate.getTime() / 1000);
+        const response = await fetch(`https://api.backpack.exchange/api/v1/klines?symbol=${market}&interval=15m&startTime=${startTime}`);
         const data = await response.json();
-        // console.log("from kline", data);
         res.json(data);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch data' });
